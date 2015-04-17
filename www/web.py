7966e5865f223172734710cada8927031ef86f22
wangyu <wayu002@sina.com>
2015-04-17 23:39:52 +0800

    add code to web.py

--- www/web.py
+++ www/web.py
@@ -108,3 +108,182 @@ _RESPONSE_STATUS = {
 
 _RE_RESPONSE_STATUS = re.compile(r'^\d\d\d(\ [\w\ ]+)?$')
 
+_RESPONSE_HEADERS = (
+    'Accept-Ranges',
+    'Age',
+    'Allow',
+    'Cache-Control',
+    'Connection',
+    'Content-Encoding',
+    'Content-Language',
+    'Content-Length',
+    'Content-Location',
+    'Content-MD5',
+    'Content-Disposition',
+    'Content-Range',
+    'Content-Type',
+    'Date',
+    'ETag',
+    'Expires',
+    'Last-Modified',
+    'Link',
+    'Location',
+    'P3P',
+    'Pragma',
+    'Proxy-Authenticate',
+    'Refresh',
+    'Retry-After',
+    'Server',
+    'Set-Cookie',
+    'Strict-Transport-Security',
+    'Trailer',
+    'Transfer-Encoding',
+    'Vary',
+    'Via',
+    'Warning',
+    'WWW-Authenticate',
+    'X-Frame-Options',
+    'X-XSS-Protection',
+    'X-Content-Type-Options',
+    'X-Forwarded-Proto',
+    'X-Powered-By',
+    'X-UA-Compatible',
+    )
+
+_RESPONSE_HEADER_DICT = dict(zip(map(lambda x: x.upper(), _RESPONSE_HEADERS)), _RESPONSE_HEADERS)
+_HEADER_X_POWERED_BY = ('X-Powered-By', 'iDou/1.0')
+
+class HttpError(Exception):
+    '''
+    HttpError define the http errors
+    '''
+    def __init__(self, code):
+        super(HttpError, self).__init__()
+        self.status = '%d %s' % (code, _RESPONSE_STATUS[code])
+
+    def header(self, name, value):
+        if not hasattr(self, '_headers'):
+            self._headers = [_HEADER_X_POWERED_BY]
+        self._headers.append((name, value))
+
+    @property
+    def headers(self):
+        if hasattr(self, '_headers'):
+            return self._headers
+        return []
+
+    def __str__(self):
+        return self.status
+
+    __repr__ = __str__
+
+class RedirectError(HttpError):
+    '''
+    define the http redirect code
+    '''
+    def __init__(self, code, location):
+        super(RedirectError, self).__init__(code)
+        self.location = location
+
+    def __str__(self):
+        return '%s %s' % (self.status, self.location)
+
+    __repr__ = __str__
+
+def badrequest():
+    '''
+    send a bad request response
+    '''
+    return HttpError(400)
+
+def unauthorized():
+    '''
+    send a unauthorized response
+    '''
+    return HttpError(401)
+
+def forbidden():
+    '''
+    send forbidden response
+    '''
+    return HttpError(403)
+
+def notfound():
+    return HttpError(404)
+
+def confilict():
+    return HttpError(409)
+
+def internalerror():
+    return HttpError(500)
+
+def redirect(location):
+    '''
+    Do permanent redirect
+    '''
+    return RedirectError(301, location)
+
+def found(location):
+    return RedirectError(302, location)
+
+def seeother(location):
+    '''
+    Do temporary redirect
+    '''
+    return RedirectError(303, location)
+
+def _to_str(s):
+    if isinstance(s, str):
+        return s
+    if isinstance(s, unicode):
+        return s.encode('utf-8')
+    return str(s)
+
+def _to_unicode(s):
+    return s.decode('utf-8')
+
+def _quote(s, encoding='utf-8'):
+    '''
+    Url quote as str.
+    >>> _quote('http://example/test?a=1+')
+    'http%3A//example/test%3Fa%3D1%2B'
+    '''
+    if isinstance(s, unicode):
+        s = s.encode(encoding)
+    return urllib.quote(s)
+
+def _unquote(s, encoding='utf-8'):
+    return urllib.unquote(s).decode(encoding)
+
+def get(path):
+    '''
+    A @get decorator.
+    >>> @get(/test/:id)
+    . . . def  test():
+    . . .   return 'ok'
+    . . .
+    >>> test.__web_route_
+    '/test/:id'
+    >>>test.__web_method_
+    'GET'
+    >>>test()
+    'ok'
+    '''
+    def _decorator(func):
+        func.__web_route_ = path
+        func.__web_method = 'GET'
+        return func
+    return _decorator
+
+def post(path):
+    def _decorator(func):
+        func.__web_route = path
+        func.__web_method = 'POST'
+        return func
+    return _decorator
+
+_re_route = re.compile(r'')
+
+
+
+
